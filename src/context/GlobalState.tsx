d096798cba6a545afcc9a440d7cb62543c31ca10
KSRS CHARAN <51869689+ksrscharan@users.noreply.github.com>
2022-06-28 17:48:42 +0530

    code format

--- src/context/GlobalState.tsx
+++ src/context/GlobalState.tsx
@@ -1,19 +1,14 @@
-import React, {
-  createContext,
-  ReactNode,
-  useReducer,
-} from "react";
+import React, { createContext, ReactNode, useReducer } from "react";
 
 import Reducer from "./Reducer";
 
-
-interface StateType{
+interface StateType {
   addTodo: string;
-  date: string
+  date: string;
   isEditing: boolean;
-  selectedEdit: {} | {id: number; date: string; title: string}
-  todo: {}[] | {id: number; date: string; title: string}[];
-  updatedTitle: string
+  selectedEdit: {} | { id: number; date: string; title: string };
+  todo: {}[] | { id: number; date: string; title: string }[];
+  updatedTitle: string;
 }
 
 const initialState: StateType | any = {
@@ -25,14 +20,13 @@ const initialState: StateType | any = {
   updatedTitle: "",
 };
 
-
 export const GlobalContext = createContext(initialState);
 
-export function GlobalProvider({ children }: {children: ReactNode}) {
+export function GlobalProvider({ children }: { children: ReactNode }) {
   const [state, dispatch] = useReducer(Reducer, initialState);
 
   return (
-    <GlobalContext.Provider value={ { state , dispatch } }>
+    <GlobalContext.Provider value={{ state, dispatch }}>
       {children}
     </GlobalContext.Provider>
   );
