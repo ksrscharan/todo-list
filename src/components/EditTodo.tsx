bdc07161bd89b1ee3a0d850627bc36cb8d953c7e
KSRS CHARAN <51869689+ksrscharan@users.noreply.github.com>
2022-06-29 13:13:21 +0530

    Feature: Edit ToDo Component Addition (#3)
    * addTodo component
    
    * Read Todo Component
    
    * Edit Todo Component Addition
    
    * code format
    
    * code format
    
    * code format

---
+++ src/components/EditTodo.tsx
@@ -0,0 +1,42 @@
+import React, { useContext } from "react";
+
+import { ACTION } from "../context/Actions";
+import { GlobalContext } from "../context/GlobalState";
+
+function EditTodo() {
+  const { state, dispatch } = useContext(GlobalContext);
+
+  return (
+    <div>
+      {state.isEditing && (
+        <div>
+          <div>EDIT TITLE</div>
+          <input
+            type="text"
+            onChange={(e) =>
+              dispatch({
+                type: ACTION.REGISTER_UPDATE,
+                payload: e.target.value,
+              })
+            }
+          />
+          <button
+            onClick={() =>
+              dispatch({
+                type: ACTION.UPDATE_TITLE,
+                payload: state.updatedTitle,
+              })
+            }
+          >
+            UPDATE
+          </button>
+          <button onClick={() => dispatch({ type: ACTION.CANCEL_UPDATE })}>
+            CANCEL
+          </button>
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default EditTodo;
